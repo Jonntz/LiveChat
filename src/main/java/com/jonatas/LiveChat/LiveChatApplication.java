4dd7835ac97d225251252f3ee638dd3ebcd33911
Jonntz <monteirojonatas23@gmail.com>
2024-11-04 19:45:30 -0300

    Feat: First commit

---
+++ src/main/java/com/jonatas/LiveChat/LiveChatApplication.java
@@ -0,0 +1,13 @@
+package com.jonatas.LiveChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveChatApplication.class, args);
+	}
+
+}
